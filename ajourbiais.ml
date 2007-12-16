1ac056e71d439e6ed495f0a75b28ef7edbdd09a9
CamaradeTux <CamaradeTux@bb158eba-3b3a-0410-941e-1d97de9ad5d4>
2007-12-16 12:43:07 +0000

    Ai déplacé énormément de fonctions en fin de ajourbiais.ml Il y a maintenant plus de 150 lignes de commentaires à la fin du fichier pour 200 de code ! alphabet.ml utilisait Ajourbiais.test alors qu'il aurait du utiliser Ajourbiais.super_test ;  de plus Ajourbiais.test fait partie des fonctions déplacées en commentaire en fin de fichier. J'ai aussi un peu modifié fft.ml mais c'était juste une variable non utilisée.

--- ajourbiais.ml
+++ ajourbiais.ml
@@ -1,50 +1,49 @@
-Gc.set {(Gc.get()) with Gc.minor_heap_size=200000};;
+Gc.set {(Gc.get()) with Gc.minor_heap_size=200000};
 
-Random.init (int_of_float (Unix.time ()));;
+Random.init (int_of_float (Unix.time ()));
 
 type neurone=
-		{poids: float array; mutable activation:float; mutable sortie:float;mutable sensib: float};;
-type reseau=neurone array array;;
+  {poids: float array; mutable activation:float; mutable sortie:float;mutable sensib: float}
+and reseau=neurone array array
 
-let t=ref 0.;;
-let scan_int () = Scanf.scanf " %d" (fun x -> x);;
-let sigmoide =fun x->1./. (1. +.exp (-.x));;
+let t=ref 0.
+let sigmoide =fun x->1./. (1. +.exp (-.x))
 let dsigm x=
   let exp_moins_x=exp (-.x) in
-    exp_moins_x /. (1. +. exp_moins_x) ** 2.
-;;
+  exp_moins_x /. (1. +. exp_moins_x) ** 2.
+let printf_tab tab=
+  for i=0 to Array.length tab -1 do Printf.printf "%f " tab.(i) done;
+  Printf.printf "\n"
+let abs x=
+  if x>0. then x else -.x;;
+
 
 let propagation entree (res:reseau) sigmoide=
-	let m=Array.length res in
-	for neur=0 to Array.length res.(0) -1 do
-                t:=0.;
-		for i=0 to Array.length entree -1 do
-			t:=!t +. (res.(0).(neur).poids.(i) *. entree.(i));
-		done;
-		res.(0).(neur).activation <- !t -. res.(0).(neur).poids.(Array.length entree); (* rajout du biais dans la premiere couche *)
-		res.(0).(neur).sortie <- sigmoide res.(0).(neur).activation
-	done;
-	for couche=1 to m-1 do
-		for neur=0 to Array.length res.(couche) -1 do
-                        t:=0.;
-                        let couche_moins_un=couche-1 in
-			for i=0 to Array.length res.(couche_moins_un) -1 do
-				t:=!t+. (res.(couche).(neur).poids.(i) *.
-                                res.(couche_moins_un).(i).sortie);
-			done;
-			res.(couche).(neur).activation <- !t -.
-                          res.(couche).(neur).poids.(Array.length res.(couche_moins_un))  ;(*rajout du biais dans la couche*)
-			res.(couche).(neur).sortie <- sigmoide res.(couche).(neur).activation
-		done
-	done
-	;;
-
-(*important !!*)
-
-(*di = oi(1-oi)(ci-oi)
-	cette formule vient d'une formule du calcul de la derivée dela sigmoide
-	cf http://www.grappa.univ-lille3.fr/polys/apprentissage/sortie005.html#toc12 *)
-
+  let m=Array.length res in
+  for neur=0 to Array.length res.(0) -1 do
+    t:=0.;
+    for i=0 to Array.length entree -1 do
+      t:=!t +. (res.(0).(neur).poids.(i) *. entree.(i));
+    done;
+    res.(0).(neur).activation <- !t -. res.(0).(neur).poids.(Array.length entree);
+      (* rajout du biais dans la premiere couche *)
+    res.(0).(neur).sortie <- sigmoide res.(0).(neur).activation
+  done;
+  for couche=1 to m-1 do
+    for neur=0 to Array.length res.(couche) -1 do
+      t:=0.;
+      let couche_moins_un=couche-1 in
+      for i=0 to Array.length res.(couche_moins_un) -1 do
+        t:=!t+. (res.(couche).(neur).poids.(i) *.
+        res.(couche_moins_un).(i).sortie);
+      done;
+      res.(couche).(neur).activation <- !t -.
+        res.(couche).(neur).poids.(Array.length res.(couche_moins_un));
+        (*rajout du biais dans la couche*)
+      res.(couche).(neur).sortie <- sigmoide res.(couche).(neur).activation
+    done
+  done
+;;
 
 
 let calcul_sensib (res:reseau) entree sortie eta=
@@ -121,86 +120,32 @@ let entrainement (reseau:reseau) tab_couples eta nb_test_max sigmoide=
 	done
 ;;
 
-let printf_tab tab=
-	for i=0 to Array.length tab -1 do Printf.printf "%f " tab.(i) done;
-	Printf.printf "\n"
-;;
-
-let test res tab_couples= (* !! valable seulement pour un neurone sur la couche de sortie*)
-	let p1,d1=tab_couples.(0) in 
-	let p,d=ref p1,ref d1 in
-	let n=Array.length tab_couples in
-	let m=Array.length res in
-	let bob=Array.make (Array.length res.(m-1) ) 0. in
-	for i=0 to  n -1 do
-		p := fst tab_couples.(i);
-		d := snd tab_couples.(i);
-		propagation !p res sigmoide;
-		for k=0 to Array.length res.(m -1)  -1 do
-			bob.(k) <- res.(m-1).(k).sortie
-		done;
-		Printf.printf "entrees        :";
-		printf_tab !p;
-		Printf.printf "sortie desiree :";
-		printf_tab !d;
-		Printf.printf "sortie reelles :";
-		printf_tab bob;
-		Printf.printf "\n----------------------------------\n";
-	done;
-;;
-
-let erreur res tab_couples= (* !! valable seulement pour un neurone sur la couche de sortie*)
-	let abs x=
-		if x>0. then x else -.x in
-	let p1,d1=tab_couples.(0) in 
-	let p,d=ref p1,ref d1 in
-	let n=Array.length tab_couples in
-	let erreur =ref 0. in
-	for i=0 to  n -1 do
-		p := fst tab_couples.(i);
-		d := snd tab_couples.(i);
-		propagation !p res sigmoide;
-		erreur := !erreur +. (abs (!d.(0)-. res.(Array.length res -1).(0).sortie));
-	done;
-	!erreur /. (float_of_int n);
-;;
-
-let abs x=
-	if x>0. then x else -.x;;
-
 let super_erreur res tab_couples (* valable pour tout type de reseau*)=
 	let p1,d1=tab_couples.(0) in 
 	let p,d=ref p1,ref d1 in
 	let n=Array.length tab_couples in
+        let long_res=Array.length res in
 	let erreur =ref 0. in
 	let erreur2=ref 0. in
 	for i=0 to  n -1 do
+		erreur:=0.;
 		p := fst tab_couples.(i);
 		d := snd tab_couples.(i);
 		propagation !p res sigmoide;
-		for j=0 to Array.length (res.(Array.length res -1)) -1 do
-			let tmp= abs (!d.(j)-. res.(Array.length res -1).(j).sortie)  in
+		for j=0 to Array.length (res.(long_res -1)) -1 do
+			let tmp= abs (!d.(j)-. res.(long_res -1).(j).sortie)  in
 			erreur := !erreur +. (tmp *. tmp);
 		done;
 		erreur2:=!erreur2 +. sqrt (!erreur);
-		erreur:=0.;
 	done;
 	!erreur2 /. (float_of_int n);
 ;;
 
-let train (res:reseau) tab_couples eta nb_test_max sigmoide=
-  let mistake=ref 0. in
-  let i=ref 0 in
-  while !i<= 100 && !mistake <> (erreur res tab_couples) do	
-    mistake := erreur res tab_couples;
-    Printf.printf "%f\n" (!mistake);
-    entrainement res tab_couples eta (nb_test_max/100) sigmoide;
-    incr i;
-  done;
-  Printf.printf "au bout de: %d\n" (!i*nb_test_max/100);
-;;
-
-let anti_poids_nul res= (*permet de sauver les meubles si les poids deviennent nuls mais fait bcp remonter lerreur*)
+(* anti_poids_nul n'est pas utilisé mais est présent en commentaire dans
+ * super_train_log_eta et donc je ne déplace pas cette fonction à la fin du
+ * fichier mais je la mets tout de même en commentaire
+ *
+ * let anti_poids_nul res= (*permet de sauver les meubles si les poids deviennent nuls mais fait bcp remonter lerreur*)
   for bob=0 to Array.length res -1 do
     for neur=0 to Array.length res.(bob) -1 do
       for w=0 to Array.length res.(bob).(neur).poids -1 do
@@ -210,8 +155,7 @@ let anti_poids_nul res= (*permet de sauver les meubles si les poids deviennent n
     done
   done
 ;;
-
-
+*)
 
 let super_train_log_eta (res:reseau) tab_couples eta nb_test_max sigmoide=
   let pas=ref eta in
@@ -264,12 +208,86 @@ let load_struct file=
 
 
 
-
-
-
   (* toutes ces fonctions ne devraient plus servir ; on les garde "parce que",
    * ensuite on les déplace dans un autre fichier et pour finir, on les dégage
    * =) *)
+
+(*
+let scan_int () = Scanf.scanf " %d" (fun x -> x)
+*)
+
+
+(* Le commentaire suivant était situé juste en dessous de la définition de
+ * propagation ; on a du le laisser parce qu'il y avait marqué "important" mais
+ * je n'ai pas trouvé de chose de la forme di= oi(1-oi)(ci-oi) dans propagation,
+ * d'où le déplacement
+(*important !!*)
+
+(*di = oi(1-oi)(ci-oi)
+	cette formule vient d'une formule du calcul de la derivée dela sigmoide
+	cf http://www.grappa.univ-lille3.fr/polys/apprentissage/sortie005.html#toc12 *)
+
+*)
+
+
+(*
+let test res tab_couples= (* !! valable seulement pour un neurone sur la couche de sortie*)
+	let p1,d1=tab_couples.(0) in 
+	let p,d=ref p1,ref d1 in
+	let n=Array.length tab_couples in
+	let m=Array.length res in
+	let bob=Array.make (Array.length res.(m-1) ) 0. in
+	for i=0 to  n -1 do
+		p := fst tab_couples.(i);
+		d := snd tab_couples.(i);
+		propagation !p res sigmoide;
+		for k=0 to Array.length res.(m -1)  -1 do
+			bob.(k) <- res.(m-1).(k).sortie
+		done;
+		Printf.printf "entrees        :";
+		printf_tab !p;
+		Printf.printf "sortie desiree :";
+		printf_tab !d;
+		Printf.printf "sortie reelles :";
+		printf_tab bob;
+		Printf.printf "\n----------------------------------\n";
+	done;
+;;
+*)
+
+
+(*
+let train (res:reseau) tab_couples eta nb_test_max sigmoide=
+  let mistake=ref 0. in
+  let i=ref 0 in
+  while !i<= 100 && !mistake <> (erreur res tab_couples) do	
+    mistake := erreur res tab_couples;
+    Printf.printf "%f\n" (!mistake);
+    entrainement res tab_couples eta (nb_test_max/100) sigmoide;
+    incr i;
+  done;
+  Printf.printf "au bout de: %d\n" (!i*nb_test_max/100);
+;;
+*)
+
+
+(*let erreur res tab_couples= (* !! valable seulement pour un neurone sur la couche de sortie*)
+	let abs x=
+		if x>0. then x else -.x in
+	let p1,d1=tab_couples.(0) in 
+	let p,d=ref p1,ref d1 in
+	let n=Array.length tab_couples in
+	let erreur =ref 0. in
+	for i=0 to  n -1 do
+		p := fst tab_couples.(i);
+		d := snd tab_couples.(i);
+		propagation !p res sigmoide;
+		erreur := !erreur +. (abs (!d.(0)-. res.(Array.length res -1).(0).sortie));
+	done;
+	!erreur /. (float_of_int n);
+;;
+*)
+
 
 (* old version, never use again
 let super_train_log (res:reseau) tab_couples eta nb_test_max sigmoide=(*pseudo logarithmique, sert juste a mieux visualiser la pente initiale*)
@@ -305,7 +323,6 @@ let super_train_log (res:reseau) tab_couples eta nb_test_max sigmoide=(*pseudo l
 *)
 
 
-
 (* sauvegarde et lit les poids avec une précision de 20 chiffres *)
 (*let save_struct_printf_scanf (reseau:reseau) file=
   let fichier=open_out file in
@@ -323,6 +340,7 @@ let super_train_log (res:reseau) tab_couples eta nb_test_max sigmoide=(*pseudo l
 close_out fichier
 ;;
 *)
+
 
 (*
 let load_struct_printf_scanf file=
