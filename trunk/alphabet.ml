43ac5c4eeee03f93f8fb6dda2437b8165cbe8dde
Asmadeus77 <Asmadeus77@bb158eba-3b3a-0410-941e-1d97de9ad5d4>
2008-01-05 12:11:06 +0000

    J'ai rajouté les 26 lettres pour alphabet.ml, c'est tout...

--- trunk/alphabet.ml
+++ trunk/alphabet.ml
@@ -1,27 +1,26 @@
-print_endline "Exécution de la fft sur a.wav...";;
-let queue_a = Fft.queue_map (Fft.re_array_of_cplx_bigarray1_norm 8800000.)
-(Fft.spectre "./alphabet/a.wav");;
-print_endline "Exécution de la fft sur b.wav...";;
-let queue_b = Fft.queue_map (Fft.re_array_of_cplx_bigarray1_norm 8800000.)
-(Fft.spectre "./alphabet/b.wav");;
-print_endline "Ffts effectuées.";;
-print_endline "Apprentissage en cours...";;
-let lgr_a = Queue.length queue_a and lgr_b = Queue.length queue_b;;
-let lgr = lgr_a + lgr_b;;
-
 open Ajourbiais
 open Affichage
+let lgr=Array.make 26 0;;
+let queues = Array.make 26 (Queue.create ());;
 
-let res=generation [|20;10;2|] 80;;
+let int_to_ascii_string i = String.make 1 (char_of_int i);;
 
-let tab_couples=Array.make lgr ([||],[||]);;
-for i=0 to lgr_a-1 do
-  tab_couples.(i) <- Queue.pop queue_a,[|0.95; 0.1|];
-(*	ignore (Queue.pop queue); *)
-done;
-for i=0 to lgr_b-1 do
-  tab_couples.(i+lgr_a) <- Queue.pop queue_b,[|0.1; 0.95|];
-(*	ignore (Queue.pop queue); *)
+for i=0 to 25 do
+queues.(i) <- Fft.queue_map (Fft.re_array_of_cplx_bigarray1_norm 8800000.) (Fft.spectre ("./alphabet/"^int_to_ascii_string(i+97)^".wav"));
+lgr.(i) <- Queue.length queues.(i)
+done;;
+
+let res=generation [|20;10;26|] 80;;
+
+let tab_couples=Array.make (Array.fold_left (fun x y -> x+y) 0 lgr) ([||],[||]);;
+
+let j=ref 0 in
+for i=0 to 25 do
+	let fin = !j + lgr.(i) in
+  while !j < fin do
+   tab_couples.(!j) <- Queue.pop queues.(i),Array.init 26 (fun k -> if k=i then 0.95 else 0.05);
+	 incr j
+  done
 done;;
 
 let tmp=super_train_log_eta res tab_couples 0.8 (600*100) sigmoide;;
