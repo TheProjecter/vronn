ece763a905f4643b77f616659873f6488a3d0e61
Asmadeus77 <Asmadeus77@bb158eba-3b3a-0410-941e-1d97de9ad5d4>
2008-02-01 17:24:41 +0000

    commented out the step changes, fixed notes.ml

--- notes.ml
+++ notes.ml
@@ -1,36 +1,68 @@
 open Reseaux
 open Affichage
-let nb_lettres = 4;;
-let lgr=Array.make nb_lettres 0;;
-let queues = Array.make nb_lettres (Queue.create ());;
+let nb_notes = 7;;
+let nb_tri = 7;;
+let lgr=Array.make nb_notes 0;;
+let queues = Array.make nb_notes (Queue.create ());;
 
-let files = Sys.readdir "notes" ;;
-Array.sort compare files;
-for i=0 to nb_lettres-1 do
-  Printf.printf "Attempting to execute fftw... %d\n" i; flush stdout;
-  queues.(i) <- Fft.queue_map (Fft.re_array_of_cplx_bigarray1_norm 88000000.) (Fft.spectre ("./notes/"^files.(i+1)));
+let files = Sys.readdir "temp" ;;
+let ordre str1 str2 = match compare (String.length str1) (String.length str2) with
+	|0 -> compare str1 str2
+	|i -> i
+in
+Array.sort ordre files;
+for i=0 to nb_notes-1 do
+  queues.(i) <- Fft.queue_map (Fft.re_array_of_cplx_bigarray1_norm 88000000.) (Fft.spectre ("./temp/"^files.(i+1)));
   lgr.(i) <- Queue.length queues.(i)
 done;;
 
+print_endline ("Done with the Fast Fourier Transform in the West ! (" ^ (string_of_int nb_notes) ^")" );;
 
-let res=generation [|10;10;2|] (Array.length (Queue.peek queues.(0)));;
+exception Trouve;;
+
+let compose elem str =
+	try
+		for i=0 to (String.length str) / 4 - 1 do
+			if String.sub str (i * 4) 3 = elem then raise Trouve
+		done; 
+		false
+	with |Trouve -> true;;
+
+
+let tri elems j = 
+	if List.mem j elems then 0.95 else 0.05
+	
+let res=generation [|10;10;nb_tri|] (Array.length (Queue.peek queues.(0)));;
 
 let tab_couples=Array.make (Array.fold_left (fun x y -> x+y) 0 lgr) ([||],[||]);;
 let j=ref 0 in
-for i=0 to nb_lettres-1 do
+for i=0 to nb_notes-1 do
 	let fin = !j + lgr.(i) in
-  while !j < fin do
-   tab_couples.(!j) <- Queue.pop queues.(i),Array.init nb_lettres (fun i -> if i=0 then 0.95 else 0.05);
-	 incr j
-  done
+	if i< nb_tri 
+	then 
+	 (while !j < fin do
+			tab_couples.(!j) <- Queue.pop queues.(i),Array.init nb_tri (tri [i]);
+			incr j
+		done)
+	else 
+	 (let lst = ref [] in 
+		for k=0 to nb_tri -1 do
+			if compose (String.sub files.(k+1) 0 3) files.(i+1) then lst:=k::(!lst)
+		done;
+		while !j < fin do
+			tab_couples.(!j) <- Queue.pop queues.(i),Array.init nb_tri (tri !lst);
+			incr j
+		done);
 done;;
+
+
 
 let tmp=super_train_log_eta res tab_couples 0.01 (6000) sigmoide;;
 
-let (_,l2)=tmp in affiche "./results/alphabet_erreur" (List.rev l2);;
+let (_,l2)=tmp in affiche "./results/notes_erreur" (List.rev l2);;
 
 (* test res tab_couples;;*)
 
 Printf.printf "Erreur %f \n\n" (super_erreur res tab_couples);;
 
-save_struct res "./results/alphabet_struct";;
+save_struct res "./results/notes_struct";;
