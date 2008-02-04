ead0317362dc38b8f1b3e73e3c9b15d301fa19ff
Asmadeus77 <Asmadeus77@bb158eba-3b3a-0410-941e-1d97de9ad5d4>
2008-02-04 15:26:31 +0000

    Managed to get less entries to the neural network to make it work better, and this works ! :D

--- xorbiais.ml
+++ xorbiais.ml
@@ -1,4 +1,4 @@
-open Tipedom
+open Reseaux
 
 (*xor avec biais*)
 let res=generation [|3;1|] 2;;
@@ -9,11 +9,11 @@ Printf.printf "Le Xor biaise :) \n\n";;
 Sys.catch_break true;
 
 (try
-ignore (super_train_log res  tab_couples 0.5 300000 sigmoide)
+ignore (super_train_log_eta res  tab_couples 0.05 300000 sigmoide)
 with
 |Sys.Break -> ());;
 
-Printf.printf "erreur : %f \n poids : " (erreur res tab_couples);
+Printf.printf "erreur : %f \n poids : " (super_erreur res tab_couples);
 for i=0 to 1 do
 	for j=0 to Array.length res.(i) -1 do
 		for k=0 to Array.length res.(i).(j).poids -1 do
