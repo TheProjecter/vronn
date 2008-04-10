c6a11b29146b9878adb099afe9b2072f55f3f627
Asmadeus77 <Asmadeus77@bb158eba-3b3a-0410-941e-1d97de9ad5d4>
2008-04-10 12:38:31 +0000

    regarde

--- regardelesbeauxcoefsdelafft.ml
+++ regardelesbeauxcoefsdelafft.ml
@@ -1,5 +1,6 @@
 open Reseaux
 open Affichage
+open Params
 
 let nbr= 1
 
@@ -12,13 +13,9 @@ in
 Array.sort ordre files;;*)
 
 let queues = Array.make nbr (Queue.create ());;
-let rec queuemap f queue = if Queue.is_empty queue then Queue.create () else let a = Queue.pop queue in let res = queuemap f queue in Queue.push (f a) res; res
-in
 
 for i=0 to nbr-1 do 
-	queues.(i) <- Fft.queue_map (Fft.array_of_res_norm_moy 8800000. 3) (Fft.spectre ("./midge/3a.wav")) (*/^files.(i+1)))*);
-	queues.(i) <- queuemap (fun tab -> Array.sub tab 0 500) queues.(i);
-
+	queues.(i) <- Fft.queue_map (fun tab -> Array.sub (Fft.array_of_res_norm_moy norme moy tab) debut_tab_fft taille_tab_fft) (Fft.spectre ("./whee.wav") dt);
 done;;
 
 (* print_endline ("Done with the Fastest Fourier Transform in the West ! (1)" );; *)
@@ -33,6 +30,7 @@ done;;
 
 for i= 0 to 0 do
 (*print_endline files.(i+1);*)
-	Queue.iter (Array.iteri (fun j x -> Printf.printf "%i %10f\n" j x)) queues.(i);
+	let count = ref 1 in
+	Queue.iter (fun tab -> let outchan=open_out ("gnuplot" ^ (string_of_int !count)) in Array.iteri (fun j x -> Printf.fprintf outchan"%i %10f\n" j x) tab; close_out outchan; incr count) queues.(i);
 	print_newline ();
 done
