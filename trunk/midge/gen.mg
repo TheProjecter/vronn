a55bafdabf6329def44e7d6af6d34a4ea0ae7425
Asmadeus77 <Asmadeus77@bb158eba-3b3a-0410-941e-1d97de9ad5d4>
2008-04-10 07:33:11 +0000

    MAJOR CHANGES You'll have to get midge to generate midi sounds, then timidity to turn them into waves. fft.ml has been fixed, it had an horrible bug from the begining... Might try again with alphabet now we know this, but working on notes still. Way to go.

---
+++ trunk/midge/gen.mg
@@ -0,0 +1,14 @@
+@head {
+	$time_sig 4/4
+	$tempo 80
+}
+
+@body {
+
+		@channel 1 {
+		$patch 74 # flute
+		$length 4
+
+		a3
+	}
+}
--- trunk/reforme.ml
+++ trunk/reforme.ml
@@ -1,34 +1,48 @@
 open Reseaux
 
-let queue = Fft.queue_map (Fft.array_of_res_norm_moy 880000. 100) (Fft.spectre ("./court.wav"))
+let seuil = 0.1
+
+let queue = 
+	let t = Fft.queue_map (Fft.array_of_res_norm_moy 8800000. 3) (Fft.spectre ("./whee.wav"))
+	in Fft.queue_map (fun tab -> Array.sub tab 0 500) t
+
 let lgr = Queue.length queue
 
-let res=load_struct "./results/notes_struct"
+let res=load_struct "./results/notes_struct_all_200"
 
 let n = Array.length res.(Array.length res -1)
 
-let notes = Queue.create ()
+let notesreconnues = Queue.create ()
 
-let files = Sys.readdir "./notes/"
+
+let gammes = (* [|"3"; "4"|] *)[|"3";"4";"5";"6"|]
+let notes = (*[|"c";"e";"g";"a"|]*)[|"c";"d";"e";"f";"g";"a";"b"|]
+let nb_notes = Array.length notes
+let nb_simples = nb_notes * Array.length gammes
+let nb_couples = (* nb_simples * 2 - 3*) nb_simples * (nb_simples - 1) / 2
+let simples = Array.init nb_simples (fun i -> gammes.(i / nb_notes) ^ notes.(i mod nb_notes))
+let couples =
+  let t= Array.make nb_couples "" and c = ref 0 in
+  (try
+	for i = 0 to nb_simples-1 do
+    for j = i+1 to nb_simples-1 do
+      t.(!c) <- simples.(i) ^ "," ^ simples.(j); incr c; if !c >= nb_couples then failwith "fin";
+    done;
+  done;
+  with Failure "fin" -> ());
+  t
+
+
+let nb_files = nb_simples + nb_couples
+let files = Array.concat [simples; couples]
 
 let _=
-	let ordre str1 str2 = match compare (String.length str1) (String.length str2) with
-  |0 -> compare str1 str2
-  |i -> i
-	in Array.sort ordre files;
 	for i= 0 to lgr-1 do
 		let t=propa (Queue.pop queue) res and lst = ref [] in
 		for j = 0 to n-1 do
-			if t.(j) > 0.6 then lst:=files.(j+1)::(!lst)
+			if t.(j) > seuil then lst:=(files.(j),t.(j))::(!lst)
 		done;
-		Queue.push !lst notes
+		Queue.push !lst notesreconnues
 	done;
 
-Queue.iter (fun x -> List.iter (Printf.printf "%s, ") x; print_newline()) notes
-
-
-
-
-
-
-
+Queue.iter (fun x-> List.iter (fun (s,i) -> Printf.printf "%s : %f, " s i) (List.rev x); print_newline()) notesreconnues
